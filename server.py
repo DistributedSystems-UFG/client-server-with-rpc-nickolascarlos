3da46987eb1426793c449f365d912efa0a612e40
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2022-12-12 15:08:53 -0300

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,34 @@ from rpyc.utils.server import ThreadedServer
 class DBList(rpyc.Service):
   value = []
 
+  # Retorna o índice da primeira ocorrência de value
+  # Caso nenhuma ocorrência seja encontrada, o valor -1 é retornado
+  def exposed_find(self, value):
+    try:
+      value_index = self.value.index(value)
+    except:
+      value_index = -1
+
+    return value_index
+
+  # Remove a primeira ocorrência de value na lista
+  def exposed_delete(self, value):
+    value_index = self.value.index(value)
+    self.value = self.value[0:value_index] + self.value[value_index+1:]
+    return self.value
+
+  # Insere data no índice `position` da lista
+  def exposed_insert(self, data, position):
+    self.value = self.value[0:position] + [data] + self.value[position:]
+    return self.value
+
   def exposed_append(self, data):
     self.value = self.value + [data]
+    return self.value
+
+  # Ordena a lista ascendente ou descendentemente
+  def exposed_sort(self, reverse = False):
+    self.value.sort(reverse=reverse)
     return self.value
 
   def exposed_value(self):
