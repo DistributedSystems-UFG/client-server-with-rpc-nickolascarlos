8408d5521b5229d23b8bcb1ba82250112d583a20
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2022-12-12 15:09:24 -0300

    Update client.py

--- client.py
+++ client.py
@@ -6,4 +6,14 @@ class Client:
   print (conn.root.exposed_value())
   conn.root.exposed_append(5)       # Call an exposed operation,
   conn.root.exposed_append(6)       # and append two elements
+  conn.root.exposed_append(10) 
+  conn.root.exposed_append(77) 
+  conn.root.exposed_append(18)
+  conn.root.exposed_insert(99, 2)
+  conn.root.exposed_append(101)
+  print (conn.root.exposed_value()) 
+  conn.root.exposed_delete(77)
+  print("Posição 17:", conn.root.exposed_find(17) if conn.root.exposed_find(17) != -1 else "NOT FOUND")
+  print (conn.root.exposed_value())   # Print the result
+  conn.root.exposed_sort()
   print (conn.root.exposed_value())   # Print the result
