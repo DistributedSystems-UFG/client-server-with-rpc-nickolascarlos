bc863f7dfc8211eb744fe08d5c7c76b38ed14e8c
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2023-01-13 10:52:22 -0300

    Update client.py

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ import rpyc
 from constRPYC import * #-
 
 class Client:
-  conn = rpyc.connect(SERVER, PORT) # Connect to the server
+  conn = rpyc.connect(SERVER, PORT)
   print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
+  conn.root.exposed_append(5)
+  conn.root.exposed_append(6)
   conn.root.exposed_append(10) 
   conn.root.exposed_append(77) 
   conn.root.exposed_append(18)
@@ -15,6 +15,6 @@ class Client:
   conn.root.exposed_delete(77)
   print("Posição do 17:", conn.root.exposed_find(17) if conn.root.exposed_find(17) != -1 else "NOT FOUND")
   print("Posição do 18:", conn.root.exposed_find(17) if conn.root.exposed_find(17) != -1 else "NOT FOUND")
-  print (conn.root.exposed_value())   # Print the result
+  print (conn.root.exposed_value())
   conn.root.exposed_sort()
-  print (conn.root.exposed_value())   # Print the result
+  print (conn.root.exposed_value())
